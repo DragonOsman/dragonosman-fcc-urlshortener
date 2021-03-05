79b9700253d7bacca112e3268b7f401047e46120
Osman Zakir <osmanzakir90@hotmail.com>
2021-03-05 06:02:26 +0500

    changed functions to use arrow syntax, added path.join instead concatenation for path in sendFile

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const BitlyClient = require("bitly").BitlyClient;
+const bitly = new BitlyClient(process.env.BITLY_ACCESS_TOKEN);
+const path = require("path");
 const app = express();
 
 // Basic Configuration
@@ -8,17 +11,17 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(path.join(`${process.cwd()}`, "/views/index.html"));
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", (req, res) => {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
